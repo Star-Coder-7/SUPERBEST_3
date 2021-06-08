b543f70441f8a06b01a8671da6a160fcbca70c42
Shiven Singhal <19singsh@hitchinboys.co.uk>
2021-06-08 21:56:31 +0100

    initial commmit

--- store/models.py
+++ store/models.py
@@ -16,7 +16,9 @@ class Product(models.Model):
     name = models.CharField(max_length=20, null=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     digital = models.BooleanField(default=False, null=True, blank=True)
+    category = models.CharField(max_length=30, null=True)
     image = models.ImageField(null=True, blank=True)
+    description = models.TextField(max_length=1000, null=True)
 
     def __str__(self):
         return self.name
--- store/views.py
+++ store/views.py
@@ -6,6 +6,22 @@ from datetime import datetime
 from .models import *
 
 
+def viewPage(request):
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
+    else:
+        items = []
+        order = {'get_cart_total': 0, 'get_cart_items': 0, 'shipping': False}
+        cartItems = order['get_cart_items']
+
+    products = Product.objects.all()
+    context = {'products': products, 'cartItems': cartItems}
+    return render(request, 'store/viewPage.html', context)
+
+
 def nonPhysical(request):
     if request.user.is_authenticated:
         customer = request.user.customer
